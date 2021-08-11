a8bf8250f030d8438087dffb2050f960452587e2
William Johannes Saaby Porsmose <will5606@edu.campusvejle.dk>
2021-08-11 09:38:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EX01HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string schoolName;
+            string motto = "Fokuseret talent";
+
+            schoolName = "AspIT - Nordjylland";
+
+            Console.WriteLine($"{schoolName}, er en skole i Aalborg. Mottoet for skolen er: {motto}.");
+
+            Console.ReadKey();
+        }
+    }
+}
